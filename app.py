7d36e9e690bafbf9816beafeb8058fc198e3b928
pryelluw <pryelluw@gmail.com>
2020-08-29 01:34:46 -0400

    adding some documentation ...

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from dice import Dice
 
 app = Flask(__name__)
 
+instructions = [
+    'Roll a dice with any side count.',
+    ' Use: Pass the number of sides the dice you want to roll has.',
+    ' Example: dice-dice-production.herokuapp.com/20',
+    ' Response: JSON',
+    ' Response Data Example: {"roll": 10}'
+]
+
+HELP = ''.join(instructions)
+
+
+@app.route('/')
+def index():
+    return jsonify({'help': HELP})
+
 
 @app.route('/<sides>')
 def roll_the_dice(sides):
